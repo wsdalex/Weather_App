33df9f3bb3069ef627f7ad9ff631b6972e00aecd
wsdalex <williama149@gmail.com>
2024-01-25 15:36:21 +0000

    Guess I can show archie

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import "./App.css";
 function App() {
   //Take user input and search for location
   //set location
+  //call geolocation api and get coordinates
+  //set coordinates
   //call weather api and get weather
   //render current weather
   const [location, setLocation] = useState([]);
@@ -14,7 +16,7 @@ function App() {
 
   const apiKey = "7f5ac72c104616aa33dd624b9f2e2a45";
   const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=3&appid=${apiKey}`;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${apiKey}`;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${apiKey}`;
 
   const getLocation = async () => {
     try {
@@ -29,8 +31,8 @@ function App() {
 
   const getWeather = async () => {
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${apiKey}`;
-      const response = await fetch(url);
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${apiKey}`;
+      const response = await fetch(weatherUrl);
       const data = await response.json();
       setWeather(data);
       console.log(data);
@@ -39,7 +41,7 @@ function App() {
     }
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(input);
     getLocation();
@@ -48,33 +50,35 @@ function App() {
   };
 
   return (
-    <div className="bg-sky-500">
-      <form
-        className="flex flex-col justify-around h-40"
-        onSubmit={handleSubmit}
-      >
-        <input
-          type="text"
-          className="bg-gray-300 mx-3"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button type="submit" className="text-white bg-sky-700 rounded mx-3">
-          Search
-        </button>
-      </form>
-      {weather ? (
-        <div className="flex flex-col items-center">
-          <h3>{location.name}</h3>
-          <img
-            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-            alt={weather.weather?.[0]?.description}
+    <div className="flex flex-row justify-center">
+      <div className="bg-sky-500 w-1/3 flex flex-col justify-center items-center rounded-md">
+        <form
+          className="flex flex-col justify-around h-40"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            className="bg-gray-300 mx-3"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
-          <h4>{weather.weather?.[0]?.description}</h4>
-        </div>
-      ) : (
-        <div>Please search above</div>
-      )}
+          <button type="submit" className="text-white bg-sky-700 rounded mx-3">
+            Search
+          </button>
+        </form>
+        {weather ? (
+          <div className="flex flex-col items-center">
+            <h3>{location.name}</h3>
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather?.[0]?.description}
+            />
+            <h4>{weather.weather?.[0]?.description}</h4>
+          </div>
+        ) : (
+          <div>Please search above</div>
+        )}
+      </div>
     </div>
   );
 }
